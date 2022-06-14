c11f67569721ba40d3adfb8f71e9621c35d4b881
alex.tylor <atylor@gmail.com>
2022-06-14 15:39:53 +0100

    remove bind.sh as no longer used

--- bash/bashrc.d/bind.sh
+++
@@ -1,7 +0,0 @@
-# # use fzf to insert a directory name
-# bind '"\C-jfd":"\C-u`f_insert_directory_name`\e\C-e\C-a\C-y\C-e"'
-# # use fzf to insert a file name
-# bind '"\C-jff":"\C-u`f_insert_file_name`\e\C-e\C-a\C-y\C-e"'
-# 
-# # Not sure on this...
-# bind -x '"\C-jos":"slack"'

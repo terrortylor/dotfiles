9db9bc1dbc3ebf6a4c3c45022df56095f8ff3f70
alex.tylor <atylor@gmail.com>
2022-07-20 17:25:14 +0100

    some bash history tunning

---
+++ bash/bashrc.d/history.sh
@@ -0,0 +1,11 @@
+# append to history, not overwrite
+shopt -s histappend
+# Attempt to save all lines of a multiple-line command in the same entry
+shopt -s cmdhist
+# Print the timestamp of each command
+HISTTIMEFORMAT='%F %T '
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$"\n"}history -a; history -c; history -r"
+# Do not store a duplicate of the last entered command
+HISTCONTROL=ignoredups
+HISTIGNORE="ls:ll:ls -al:pwd"

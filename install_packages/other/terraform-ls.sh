3ad4b1410c872c4390cb17b92d244a09f2efed9f
alex.tylor <atylor@gmail.com>
2022-11-03 17:10:31 +0000

    feat: update terrafrom install for osx and linux

--- install_packages/other/terraform-ls.sh
+++ install_packages/other/terraform-ls.sh
@@ -7,10 +7,16 @@ IS_UPTO_DATE=false
 VERSION=$(curl -sL https://api.github.com/repos/hashicorp/terraform-ls/releases/latest | jq -r ".tag_name")
 TRIMMED_VERSION="${VERSION##v}" # strip v from front of version
 
+if [[ $OSTYPE == 'darwin'* ]]; then
+  suffix="darwin_arm64"
+else
+  suffix="linux_amd64"
+fi
+
 if command -v terraform-ls &> /dev/null; then
   EXISTING_VERSION=$(terraform-ls --version)
   if [ "${EXISTING_VERSION}" != "${TRIMMED_VERSION}" ]; then
-    mv /usr/local/bin/terraform-ls "/usr/local/bin/terraform-ls-${EXISTING_VERSION}"
+    sudo mv /usr/local/bin/terraform-ls "/usr/local/bin/terraform-ls-${EXISTING_VERSION}"
   else
     echo "Version is upto data: ${TRIMMED_VERSION}"
     IS_UPTO_DATE=true
@@ -19,9 +25,10 @@ fi
 
 if ! ${IS_UPTO_DATE}; then
   echo "Downloading version: $VERSION"
-  wget "https://github.com/hashicorp/terraform-ls/releases/download/${VERSION}/terraform-ls_${TRIMMED_VERSION}_linux_amd64.zip"
+  wget "https://github.com/hashicorp/terraform-ls/releases/download/${VERSION}/terraform-ls_${TRIMMED_VERSION}_${suffix}.zip"
+  sudo mkdir -p /usr/local/bin
 
-  unzip "terraform-ls_${TRIMMED_VERSION}_linux_amd64.zip"
+  unzip "terraform-ls_${TRIMMED_VERSION}_${suffix}.zip"
 
   sudo mv terraform-ls /usr/local/bin/.
 fi

19c7d2f241e5e38183a44da96832c4214494793f
alex.tylor <atylor@gmail.com>
2024-06-06 08:40:58 +0100

    chore: some lazy loading to speed zsh up

--- zsh/zshrc.d/03_sdk.sh
+++ zsh/zshrc.d/03_sdk.sh
@@ -1,2 +1,5 @@
 export SDKMAN_DIR="${HOME}/.sdkman"
+
+ lazy_load 'sdk' 'kotlin' 'gradle' 'mvn' <<- 'EOF'
  [[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
+EOF
